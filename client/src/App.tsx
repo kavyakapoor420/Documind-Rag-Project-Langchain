f5acb97c2353323a2da90bbd7995027e7f9af327
kavyakapoor420 <kavyakapoor412@gmail.com>
2026-01-11 00:23:16 +0530

    created answer and landing page section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ import { Navbar } from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QuerySection } from "./components/QuerySection";
 import { UploadSection } from "./components/UploadSection";
+import HomePage from "./pages/HomePage";
+import { AnswerSection } from "./components/AnswerSection";
 
 function App() {
   return (
@@ -12,10 +14,12 @@ function App() {
         
         <BrowserRouter>
            <Navbar/>
+           <AnswerSection/>
            <QuerySection/>
             <Routes>
                <Route path="/" element={<div>Home Page</div>} />
                <Route path="/upload" element={<UploadSection/>}/>
+               <Route path="/hello" element={<HomePage/>}/>
             </Routes>
         </BrowserRouter>
       </div>
